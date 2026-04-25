e2d79c6bdf7ffc259694ce6ddfdfa59abcc01d0c
league <league@wts14-3.attlocal.net>
2026-04-25 12:57:12 -0700

    aaaaaaaaaaaaaahaahahahhhhhh

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -15,11 +15,26 @@ public class Validation {
 		int randomNumber = randomMaker.nextInt(5);
 		
 		System.out.println(randomNumber);
-
+ 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
-		// 2. Repeat all the code above 10 times
-		
+	    if (randomNumber == 0) {
+        	JOptionPane.showMessageDialog(null, "You are smart!");
+    	}
+        else if (randomNumber == 1) {
+        	JOptionPane.showMessageDialog(null, "You are cool!");
+    	}
+        else if (randomNumber == 2) {
+        	JOptionPane.showMessageDialog(null, "you smell good (I checked)");
+    	}
+        else if (randomNumber == 3) {
+        	JOptionPane.showMessageDialog(null, "your feet are well sized");
+    	}
+        else if (randomNumber == 4) {
+        	JOptionPane.showMessageDialog(null, "You are somewhat important");
+    	}
+        else {
+        	JOptionPane.showMessageDialog(null, "You have good hygene");
+        }
 		// 3. Find someone to test out your program. They will like it :)
 	}
 }
--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -6,12 +6,16 @@ package _01_random._5_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below
-
+	public static void main(String[] args) {
+		
+	}
 	// 2. Get the user to enter a question for the 8 ball to answer
-	
+	String question =JOptionPane.showInputDialog("what seems to be your question today?");
 
 	// 3. Make a variable and initialize it to a random number.
 	//     ** You will need to make a random object!
